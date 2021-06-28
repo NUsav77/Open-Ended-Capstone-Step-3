2de8519c455960cf5c729e31d96710aaa39eeecc
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-06-27 19:54:59 -0700

    Added twitter_data.py

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Open-Ended Capstone Step 3
 Next Generation Weather Radar (NEXRAD) network releases public datasets of both real-time and archival datasets in AWS S3 Buckets.
 
-This project utilizes nexradaws module, boto3 SDK, and pytz library to access the datasets.
+This project utilizes nexradaws module, boto3 SDK, tweepy, searchtweets, and pytz library. Pandas dataframe is used to print Twitter query results.
 
 ## Installation for Archive Bucket Access
 
@@ -20,3 +20,4 @@ In terminal
 1. run command ```pip install searchtweets```
 2. run command ```pip install searchtweets-v2```
 3. run command ```pip install tweepy```
+4. run command ```pip install pandas```
--- twitter_data.py
+++ twitter_data.py
@@ -1,22 +1,44 @@
 # from searchtweets import ResultStream, gen_request_parameters, load_credentials
-#
-# premium_search_args = load_credentials('~/.twitter_keys.yaml',
-#                                        yaml_key='search_tweets_v2',
-#                                        env_overwrite=False)
-#
-# query = gen_request_parameters('weather', results_per_call=10)
-# print(query)
-
-
-
+import secrets  # contains Twitter Dev keys in local
 import tweepy
-import secrets
+import pandas as pd
+
+
 
 auth = tweepy.OAuthHandler(secrets.API_Key, secrets.API_Secret_Key)
 auth.set_access_token(secrets.Access_Token, secrets.Access_Token_Secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+# Cursor object allows us to navigate through Twitter
+cursor = tweepy.Cursor(api.user_timeline, id='weatherchannel', tweet_mode='extended').items(1)
+
+# print dir to get the information contained within a single tweet
+[print(dir(i)) for i in cursor]
+
+# print full text of a tweet
+[print(i.full_text) for i in cursor]
+
+
+
+# Cursor object searching for 'tropical storm'
+storm_q_cursor = tweepy.Cursor(api.search, q='tropical storm', tweet_mode='extended').items(1)
+
+# print full text of a tweet
+[print(i.full_text) for i in storm_q_cursor]
+
+
+
+# Get full information on a single tweet
+number_of_tweets = 10
+tweets = []
+likes = []
+time = []
+
+for tweet in tweepy.Cursor(api.search, q='evacuate', tweet_mode='extended').items(number_of_tweets):
+    tweets.append(tweet.full_text)
+    likes.append(tweet.favorite_count)
+    time.append(tweet.created_at)
+
+df = pd.DataFrame({'tweets':tweets,'likes':likes,'time':time})
+print(df)
